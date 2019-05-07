80cf2b21531d69163a044a79cd80d268f838d4c3
Kedyn Macedonio <mkedyn@gmail.com>
2019-05-07 03:59:48 -0700

    fixed ai getwinner and reset for game scene

--- game/ai.py
+++ game/ai.py
@@ -12,7 +12,8 @@ class AI:
 
     def move(self, board):
         best = self.minimax(board, len(self.getEmptyIndexies(board)), True)
-
+        print(self.getEmptyIndexies(board))
+        print(board)
         if best[1] != -1:
             board[best[1]] = self.piece
 
@@ -76,15 +77,15 @@ class AI:
             return board[0]
         elif board[0] == board[4] and board[4] == board[8] and board[0] != PIECE.NONE:
             return board[0]
-        elif board[1] == board[4] and board[4] == board[7] and board[0] != PIECE.NONE:
+        elif board[1] == board[4] and board[4] == board[7] and board[1] != PIECE.NONE:
             return board[1]
-        elif board[2] == board[4] and board[4] == board[6] and board[0] != PIECE.NONE:
+        elif board[2] == board[4] and board[4] == board[6] and board[2] != PIECE.NONE:
             return board[2]
-        elif board[2] == board[5] and board[5] == board[8] and board[0] != PIECE.NONE:
+        elif board[2] == board[5] and board[5] == board[8] and board[2] != PIECE.NONE:
             return board[2]
-        elif board[3] == board[4] and board[4] == board[5] and board[0] != PIECE.NONE:
+        elif board[3] == board[4] and board[4] == board[5] and board[3] != PIECE.NONE:
             return board[3]
-        elif board[6] == board[7] and board[7] == board[8] and board[0] != PIECE.NONE:
+        elif board[6] == board[7] and board[7] == board[8] and board[6] != PIECE.NONE:
             return board[6]
 
         return PIECE.NONE
--- game/game_scene.py
+++ game/game_scene.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 from framework.scene import Scene
 from framework.text import Text
@@ -35,38 +36,58 @@ class GameScene(Scene):
         if not self.waiting_for_ai:
             self.key_pressed = key
 
+    def checkGameEnded(self):
+        score = self.ai.getBoardScoreForPiece(self.board.pieces)
+
+        if score == 7:
+            self.board.player_two.score += 1
+            time.sleep(3)
+            return True
+        elif score == -7:
+            self.board.player_one.score += 1
+            time.sleep(3)
+            return True
+        elif score == 0 and not self.ai.getEmptyIndexies(self.board.pieces):
+            time.sleep(3)
+            return True
+
+        return False
+
     def update(self):
-        if self.waiting_for_ai:
-            self.board.pieces = self.ai.move(self.board.pieces)
-            self.waiting_for_ai = False
-        elif self.key_pressed:
-            if self.key_pressed == pygame.K_1 and self.board.pieces[0] == PIECE.NONE:
-                self.board.pieces[0] = self.board.player_one.piece
-                self.waiting_for_ai = True
-            elif self.key_pressed == pygame.K_2 and self.board.pieces[1] == PIECE.NONE:
-                self.board.pieces[1] = self.board.player_one.piece
-                self.waiting_for_ai = True
-            elif self.key_pressed == pygame.K_3 and self.board.pieces[2] == PIECE.NONE:
-                self.board.pieces[2] = self.board.player_one.piece
-                self.waiting_for_ai = True
-            elif self.key_pressed == pygame.K_4 and self.board.pieces[3] == PIECE.NONE:
-                self.board.pieces[3] = self.board.player_one.piece
-                self.waiting_for_ai = True
-            elif self.key_pressed == pygame.K_5 and self.board.pieces[4] == PIECE.NONE:
-                self.board.pieces[4] = self.board.player_one.piece
-                self.waiting_for_ai = True
-            elif self.key_pressed == pygame.K_6 and self.board.pieces[5] == PIECE.NONE:
-                self.board.pieces[5] = self.board.player_one.piece
-                self.waiting_for_ai = True
-            elif self.key_pressed == pygame.K_7 and self.board.pieces[6] == PIECE.NONE:
-                self.board.pieces[6] = self.board.player_one.piece
-                self.waiting_for_ai = True
-            elif self.key_pressed == pygame.K_8 and self.board.pieces[7] == PIECE.NONE:
-                self.board.pieces[7] = self.board.player_one.piece
-                self.waiting_for_ai = True
-            elif self.key_pressed == pygame.K_9 and self.board.pieces[8] == PIECE.NONE:
-                self.board.pieces[8] = self.board.player_one.piece
-                self.waiting_for_ai = True
+        if not self.checkGameEnded():
+            if self.waiting_for_ai:
+                self.board.pieces = self.ai.move(self.board.pieces)
+                self.waiting_for_ai = False
+            elif self.key_pressed:
+                if self.key_pressed == pygame.K_1 and self.board.pieces[0] == PIECE.NONE:
+                    self.board.pieces[0] = self.board.player_one.piece
+                    self.waiting_for_ai = True
+                elif self.key_pressed == pygame.K_2 and self.board.pieces[1] == PIECE.NONE:
+                    self.board.pieces[1] = self.board.player_one.piece
+                    self.waiting_for_ai = True
+                elif self.key_pressed == pygame.K_3 and self.board.pieces[2] == PIECE.NONE:
+                    self.board.pieces[2] = self.board.player_one.piece
+                    self.waiting_for_ai = True
+                elif self.key_pressed == pygame.K_4 and self.board.pieces[3] == PIECE.NONE:
+                    self.board.pieces[3] = self.board.player_one.piece
+                    self.waiting_for_ai = True
+                elif self.key_pressed == pygame.K_5 and self.board.pieces[4] == PIECE.NONE:
+                    self.board.pieces[4] = self.board.player_one.piece
+                    self.waiting_for_ai = True
+                elif self.key_pressed == pygame.K_6 and self.board.pieces[5] == PIECE.NONE:
+                    self.board.pieces[5] = self.board.player_one.piece
+                    self.waiting_for_ai = True
+                elif self.key_pressed == pygame.K_7 and self.board.pieces[6] == PIECE.NONE:
+                    self.board.pieces[6] = self.board.player_one.piece
+                    self.waiting_for_ai = True
+                elif self.key_pressed == pygame.K_8 and self.board.pieces[7] == PIECE.NONE:
+                    self.board.pieces[7] = self.board.player_one.piece
+                    self.waiting_for_ai = True
+                elif self.key_pressed == pygame.K_9 and self.board.pieces[8] == PIECE.NONE:
+                    self.board.pieces[8] = self.board.player_one.piece
+                    self.waiting_for_ai = True
+        else:
+            self.reset()
 
     def render(self):
         super().render()
